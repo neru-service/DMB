7ab96aeb80de2cd780e34cc16e883d29d8e641d5
neru-service <95429724+neru-service@users.noreply.github.com>
2021-12-03 20:50:39 +0900

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "OTE1OTQwNTE3MzcwNTYwNTky.Yai6FA.nJ3v-bx6rbQSGiP2JDacdT91_y8", //Discord Bot Token
+  Token: process.env.Token || "OTE1OTQwNTE3MzcwNTYwNTky.Yai6FA.7iwNChkxZB8vDJatBkuIAKSgtWs", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "915940517370560592", //Discord Client ID
   ClientSecret: process.env.Discord_ClientSecret || "PkmNIW0UuubJzoYiIyWadRjs9-DA3D5k", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
